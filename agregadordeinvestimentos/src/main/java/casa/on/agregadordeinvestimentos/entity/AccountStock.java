7e10582fb6b69df2890e244fa2157ceb5158882c
Caua emanuel <emanuellcaua8@gmail.com>
2025-04-19 10:12:25 -0300

    stock

---
+++ agregadordeinvestimentos/src/main/java/casa/on/agregadordeinvestimentos/entity/AccountStock.java
@@ -0,0 +1,65 @@
+package casa.on.agregadordeinvestimentos.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_accounts_stocks")
+public class AccountStock {
+
+    @EmbeddedId
+    private AccountStockId id;
+
+    @ManyToOne
+    @MapsId("stockId")
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+
+    @ManyToOne
+    @MapsId("accountId")
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    public AccountStock(){}
+
+    public AccountStock(AccountStockId id, Stock stock, Account account, Integer quantity) {
+        this.id = id;
+        this.stock = stock;
+        this.account = account;
+        this.quantity = quantity;
+    }
+
+    public AccountStockId getId() {
+        return id;
+    }
+
+    public void setId(AccountStockId id) {
+        this.id = id;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
--- agregadordeinvestimentos/src/main/java/casa/on/agregadordeinvestimentos/entity/Account_Stock.java
+++
@@ -1,7 +0,0 @@
-package casa.on.agregadordeinvestimentos.entity;
-
-public class Account_Stock {
-
-    private int account_id;
-    private int stock_id;
-}
